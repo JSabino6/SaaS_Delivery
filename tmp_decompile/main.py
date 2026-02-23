ea5809ebb54f1999be95d97ac0155ccb66a34818
Joao Sabino <joaovitor8714822@hotmail.com>
2026-02-23 18:29:14 -0300

    Remoção de multiplos arquivos

--- tmp_decompile/main.py
+++
@@ -1,6 +0,0 @@
-# decompyle3 version 3.9.3
-# Python bytecode version base 3.10 (3439)
-# Decompiled from: Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)]
-# Embedded file name: /app/main.py
-# Compiled at: 2025-12-27 00:41:45
-# Size of source mod 2**32: 8382 bytes
